0ebca82ed7d08144086e8f5a0b7c62b2d6898039
ekhlusov <ekhlusov@gmail.com>
2019-08-28 22:24:48 +0300

    Updates

---
+++ precache-manifest.d5bfd080556e2349060a3808a5ed17d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8031f12e9080c455b03cba8a70a47311",
+    "url": "/udemy-courses-react-monsters/index.html"
+  },
+  {
+    "revision": "1412315bba38798cf47f",
+    "url": "/udemy-courses-react-monsters/static/css/main.a2cf1a21.chunk.css"
+  },
+  {
+    "revision": "a9690e2a70e33c66e568",
+    "url": "/udemy-courses-react-monsters/static/js/2.3df813ea.chunk.js"
+  },
+  {
+    "revision": "1412315bba38798cf47f",
+    "url": "/udemy-courses-react-monsters/static/js/main.8e9eccb5.chunk.js"
+  },
+  {
+    "revision": "65c607e27ad7f64ec6d9",
+    "url": "/udemy-courses-react-monsters/static/js/runtime~main.8a0c872e.js"
+  }
+]);
